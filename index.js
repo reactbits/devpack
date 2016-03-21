a9b7595f45947963eaa58b5a8bf94e7092a2d193
sergeyt <stodyshev@gmail.com>
2016-03-21 19:27:22 +0600

    making it usable

---
+++ index.js
@@ -0,0 +1,59 @@
+const _ = require('lodash');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const webpack = require('webpack');
+const makeConfig = require('./webpack');
+
+function start(options) {
+	const port = options.port || process.env.PORT || 8000;
+	const config = options.webpack || makeConfig();
+	const cwd = options.cwd || process.cwd();
+
+	const app = express();
+	const compiler = webpack(config);
+
+	app.use(morgan('dev'));
+	app.use(cors());
+
+	app.use(require('webpack-dev-middleware')(compiler, {
+		noInfo: true,
+		publicPath: config.output.publicPath,
+		stats: 'errors-only',
+	}));
+
+	app.use(require('webpack-hot-middleware')(compiler));
+
+	if (_.isFunction(options.extendApp)) {
+		options.extendApp(app);
+	}
+
+	if (_.isFunction(options.api)) {
+		app.use(options.api());
+	} else if (_.isObject(options.api)) {
+		app.use(options.api);
+	}
+
+	app.use(express.static(cwd));
+
+	// TODO allow to override index route (isomorphic apps)
+	// otherwise serve index.html
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(cwd, 'index.html'));
+	});
+
+	app.listen(port, '0.0.0.0', (err) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log('Listening at http://0.0.0.0:%s', port);
+	});
+}
+
+module.exports = {
+	startServer: start,
+	makeWebpackConfig: makeConfig,
+};
---
+++ webpack.js
@@ -0,0 +1,83 @@
+const _ = require('lodash');
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// TODO production config
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+// TODO allow to customize css loader
+const cssLoader = 'css?sourceMap&modules&importLoaders=1&localIdentName=[local]';
+
+// TODO customize loaders
+
+module.exports = function makeConfig(options) {
+	if (!options) options = {}; // eslint-disable-line
+
+	const plugins = [
+		new ExtractTextPlugin('styles.css', { allChunks: true }),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify(NODE_ENV),
+			},
+		}),
+	];
+
+	if (options.jquery === true) {
+		plugins.push(new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+		}));
+	}
+
+	const entry = _.isArray(options.entry) ? options.entry : [
+		'webpack-hot-middleware/client',
+		_.isString(options.entry) ? options.entry : './src/index',
+	];
+
+	const config = {
+		devtool: 'source-map',
+		entry: entry, // eslint-disable-line
+		output: {
+			path: path.join(__dirname, 'static'),
+			filename: 'bundle.js',
+			publicPath: '/static/',
+		},
+		plugins: plugins, // eslint-disable-line
+		module: {
+			loaders: [
+				{
+					test: /\.json$/,
+					loader: 'json',
+				},
+				{
+					test: /\.jsx?$/,
+					loader: 'babel',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.(scss|css)$/,
+					loader: ExtractTextPlugin.extract('style', [cssLoader, 'postcss', 'sass?sourceMap']),
+				},
+				{
+					test: /\.less$/,
+					loader: ExtractTextPlugin.extract('style', [cssLoader, 'postcss', 'less?sourceMap']),
+				},
+				{
+					test: /\.(ttf|eot|svg|woff(2)?)$/,
+					loader: 'file-loader',
+				},
+			],
+		},
+		resolve: {
+			extensions: ['', '.js', '.jsx', '.json', '.css', '.scss'],
+		},
+		postcss: [autoprefixer],
+	};
+
+	return _.merge(config, options.config || {});
+};
